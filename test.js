da918843bfd9b80a46636624d29c412f4fe47718
Chuang Ping Feng <nick1092387456@gmail.com>
2024-06-20 15:07:36 +0800

    [fixed] 修正注音'ㄩ'轉換pinyin不正確的問題

--- test.js
+++ test.js
@@ -1,10 +1,21 @@
-const { fromPinyin, toPinyin } = require("zhuyin-improved")
+const { fromPinyin, toPinyin } = require("./lib/cjs/index")
 
-const data = "fú fú wù"
-const data2 = "ㄈㄨˊ ㄨˋ"
+// const data = "fú fú wù"
+// const data2 = "ㄈㄨˊ ㄨˋ"
 
-const result = fromPinyin(data)
-const result2 = toPinyin(data2)
+// const result = fromPinyin(data)
+// const result2 = toPinyin(data2)
 
-console.log(result)
-console.log(result2)
+// console.log(result)
+// console.log(result2)
+
+console.log("-----")
+
+const data3 = "nǚ ér"
+const data4 = "ㄋㄩˇ ㄦˊ"
+
+const result3 = fromPinyin(data3)
+const result4 = toPinyin(data4)
+
+console.log(result3)
+console.log(result4)

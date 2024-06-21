2974acc48def57473b1cb9068306530dc792dd80
Chuang Ping Feng <nick1092387456@gmail.com>
2024-06-21 18:42:12 +0800

    [fixed] 修正輕聲檢測及轉換的位置應該是在注音前方

---
+++ ISSUE.md
@@ -0,0 +1,9 @@
+## [1.0.5] - 2024-06-21
+
+### ISSUED 10501
+
+- 當輕聲後方注音未用空格區分時，在轉換成pinyin後，後方第一個zhuyin或pinyin會被消失。
+
+### ISSUED 10502
+
+- 還沒將 CHANGELOG 1.0.5 的改動為套用到 esm module。
--- test.js
+++ test.js
@@ -1,32 +1,14 @@
 const { fromPinyin, toPinyin } = require("./lib/cjs/index")
 
-// const data = "fú fú wù"
-// const data2 = "ㄈㄨˊ ㄨˋ"
+console.log(fromPinyin("fú wù"))
+console.log(toPinyin("ㄈㄨˊ ㄨˋ"))
 
-// const result = fromPinyin(data)
-// const result2 = toPinyin(data2)
+console.log(fromPinyin("nǚ"))
+console.log(toPinyin("ㄋㄩˇ ㄦˊ"))
 
-// console.log(result)
-// console.log(result2)
+console.log(fromPinyin("lù"))
+console.log(toPinyin("ㄌㄨˋ"))
 
-// console.log("-----")
-
-const data3 = "nǚ"
-const data4 = "ㄋㄩˇ ㄦˊ"
-
-const result3 = fromPinyin(data3)
-const result4 = toPinyin(data4)
-
-console.log(result3)
-console.log(result4)
-
-console.log("-----")
-
-const data5 = "lù"
-const data6 = "ㄌㄨˋ"
-
-const result5 = fromPinyin(data5)
-const result6 = toPinyin(data6)
-
-console.log(result5)
-console.log(result6)
+console.log(fromPinyin(toPinyin("ㄗㄣˇ ˙ㄇㄜ ㄧㄤˋ").join(" ")))
+console.log(toPinyin("ㄗㄣˇ ˙ㄇㄜ ㄧㄤˋ"))
+console.log(fromPinyin("zěn me yàng"))
